57b9baf8f4042a8ae078e6e88f9dcd3acae6c839
usman <usman_pervaiz@live.com>
2018-04-04 20:58:31 +0300

    Date setup for date field

--- app/src/main/java/com/example/usman/proretro/app/FirstActivity.java
+++ app/src/main/java/com/example/usman/proretro/app/FirstActivity.java
@@ -1,9 +1,17 @@
 package com.example.usman.proretro.app;
 
+import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -15,6 +23,12 @@ import com.example.usman.proretro.interfaces.Api;
 import com.example.usman.proretro.models.Location;
 import com.example.usman.proretro.models.User;
 
+import java.text.DateFormat;
+import java.text.FieldPosition;
+import java.text.ParsePosition;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import retrofit2.Call;
@@ -29,6 +43,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class FirstActivity extends AppCompatActivity {
 
+    private BroadcastReceiver broadcastReceiver;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -41,7 +56,7 @@ public class FirstActivity extends AppCompatActivity {
         final EditText evail = (EditText) findViewById(R.id.email);
 
         Button editin = (Button) findViewById(R.id.btEdt);
-       // Button addAdd = (Button) findViewById(R.id.add);
+        // Button addAdd = (Button) findViewById(R.id.add);
         Button userS = (Button) findViewById(R.id.user);
         Button addFu = (Button) findViewById(R.id.btnAddFuel);
         Button deleteBt = (Button) findViewById(R.id.deleteButton);
@@ -63,7 +78,7 @@ public class FirstActivity extends AppCompatActivity {
         editin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                 startActivity(new Intent(FirstActivity.this, UpdateFuel.class));
+                startActivity(new Intent(FirstActivity.this, UpdateFuel.class));
             }
         });
         deleteBt.setOnClickListener(new View.OnClickListener() {
@@ -80,8 +95,94 @@ public class FirstActivity extends AppCompatActivity {
             }
         });
 
+        Intent i = new Intent(getApplicationContext(), ServiceClass.class);
+        startService(i);
+    }
+
+    private boolean runtime_permissions() {
+        if (Build.VERSION.SDK_INT >= 23 && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 100);
+
+            return true;
+        }
+        return false;
+    }
 
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 100) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Welcome", Toast.LENGTH_SHORT).show();
+            } else {
+                runtime_permissions();
+            }
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (broadcastReceiver == null) {
+            broadcastReceiver = new BroadcastReceiver() {
+                @Override
+                public void onReceive(Context context, Intent intent) {
+
+                    double lat = (double) intent.getExtras().get("lat");
+                    double langi = (double) intent.getExtras().get("lang");
+
+                    Calendar calendar = Calendar.getInstance();
+                    SimpleDateFormat mdformat = new SimpleDateFormat("yyyy / MM / dd ");
+                    String strDate = "Current Date : " + mdformat.format(calendar.getTime());
+
+                    Location loca = new Location("1",lat, langi, strDate);
+                    sendNetworkRequest(loca);
+
+
+
+                }
+            };
+        }
+        registerReceiver(broadcastReceiver, new IntentFilter("location_update"));
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (broadcastReceiver != null) {
+            unregisterReceiver(broadcastReceiver);
+        }
+    }
+
+    private void sendNetworkRequest(Location location) {
+
+
+      //  Retrofit retrofit = new Retrofit.Builder().baseUrl("https://fuel-hero.herokuapp.com/")
+        Retrofit retrofit = new Retrofit.Builder().baseUrl("http://192.168.0.103:8080/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        Api apiService = retrofit.create(Api.class);
+
+        Api api = retrofit.create(Api.class);
+        Call<List<Location>> call = api.addLocations(location);
+        call.enqueue(new Callback<List<Location>>() {
+            @Override
+            public void onResponse(Call<List<Location>> call, Response<List<Location>> response) {
+                Toast.makeText(FirstActivity.this, "sendingLoc in db", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(Call<List<Location>> call, Throwable t) {
+                Toast.makeText(FirstActivity.this, "check if it goes", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
        /* @Override
         public void onLocationChanged(Location location) {
 
@@ -110,32 +211,8 @@ public class FirstActivity extends AppCompatActivity {
                 }
             }*/
 
-      /*  private void sendNetworkRequest(Location location) {
 
-            Retrofit retrofit = new Retrofit.Builder().baseUrl("https://fuel-hero.herokuapp.com/")
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .build();
-
-        Api apiService = retrofit.create(Api.class);
-
-        Call<List<Location>> call = apiService.addLocations(location);
-
-
-
-            Api api = retrofit.create(Api.class);
-            Call<List<Location>> call = api.addLocations(location);
-            call.enqueue(new Callback<List<Location>>() {
-                @Override
-                public void onResponse(Call<List<Location>> call, Response<List<Location>> response) {
-
-                }
-
-                @Override
-                public void onFailure(Call<List<Location>> call, Throwable t) {
-
-                }
-            }*/
-    }
+  //  }
 
 
 
@@ -154,7 +231,7 @@ public class FirstActivity extends AppCompatActivity {
               //  sendNetworkRequest(user);
             }
         });*/
-    }
+
        /* private void sendNetworkRequest(User user) {
         //Heroku server is being used so it was only for localhost
             Retrofit retrofit = new Retrofit.Builder().baseUrl("http://192.168.0.103:8080/")
