acbb3ee8f40e0b16e996f1d5cbc725bf72bc421f
usman <usman_pervaiz@live.com>
2018-04-10 18:15:20 +0300

    Activity for Field Id

---
+++ app/src/main/java/com/example/usman/proretro/app/IdFieldActivity.java
@@ -0,0 +1,10 @@
+package com.example.usman.proretro.app;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by usman on 10/04/2018.
+ */
+
+public class IdFieldActivity extends AppCompatActivity {
+}
