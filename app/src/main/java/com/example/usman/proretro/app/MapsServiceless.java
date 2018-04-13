643669c9d308906042774e2e7f6a84325cb7f98c
usman <usman_pervaiz@live.com>
2018-04-14 00:48:35 +0300

    LocationService separated from FirstActivity

--- app/src/main/java/com/example/usman/proretro/app/MapsServiceless.java
+++ app/src/main/java/com/example/usman/proretro/app/MapsServiceless.java
@@ -1,8 +1,179 @@
 package com.example.usman.proretro.app;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+import com.example.usman.proretro.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.util.List;
+
 /**
  * Created by usman on 13/04/2018.
  */
 
-public class MapsServiceless {
+public class MapsServiceless extends AppCompatActivity implements OnMapReadyCallback, LocationListener {
+
+    private GoogleMap mMap;
+    private LocationManager locationManager;
+    private String provider;
+    double lat = 60.179890, lng = 24.960942;
+    boolean isExistingPoint = false;
+    Toolbar toolbar;
+
+    private Polyline route = null;
+    private PolylineOptions routeOpts = null;
+    private boolean drawTrack = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        Intent intent = getIntent();
+        toolbar = (Toolbar) findViewById(R.id.refresh);
+        getSupportActionBar().setTitle("Map");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        Criteria criteria = new Criteria();
+        provider = locationManager.getBestProvider(criteria, false);
+
+        if (ContextCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MapsServiceless.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION}, 0);
+        } else {
+            Location location = locationManager.getLastKnownLocation(provider);
+
+            locationManager.requestLocationUpdates(provider, 0, 0, this);
+            if (location != null) {
+                onLocationChanged(location);
+            }
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        LatLng position = new LatLng(lat, lng);
+        Toast.makeText(this, "" + position, Toast.LENGTH_SHORT).show();
+
+        //following polylines
+        routeOpts = new PolylineOptions().color(Color.BLUE)
+                .width(3)
+                .geodesic(true);
+        route = mMap.addPolyline(routeOpts);
+        route.setVisible(drawTrack);
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                new LatLng(lat, lng), 1));
+
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(40), 2000, null);
+
+        if (ContextCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MapsServiceless.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION}, 0);
+        } else {
+            mMap.setMyLocationEnabled(true);
+
+            mMap.getCameraPosition();
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(position));
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(position, 17));
+
+        }
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+        lat = location.getLatitude();
+        lng = location.getLongitude();
+
+        if (mMap != null) {
+            LatLng position = new LatLng(lat, lng);
+
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(lat, lng)));
+
+            mMap.animateCamera(CameraUpdateFactory.zoomTo(25));
+            if (routeOpts != null) {
+                LatLng newPoint = new LatLng(location.getLatitude(), location.getLongitude());
+                List<LatLng> points = route.getPoints();
+                points.add(newPoint);
+                route.setPoints(points);
+            }
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (ContextCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MapsServiceless.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION}, 0);
+        } else {
+            locationManager.removeUpdates(this);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (ContextCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MapsServiceless.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION}, 0);
+        } else {
+            locationManager.requestLocationUpdates(provider, 0, 0, this);
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {    }
+    @Override
+    public void onProviderEnabled(String s) {    }
+    @Override
+    public void onProviderDisabled(String s) {    }
 }
--- app/src/main/java/com/example/usman/proretro/app/TrackingOnSide.java
+++ app/src/main/java/com/example/usman/proretro/app/TrackingOnSide.java
@@ -4,25 +4,17 @@ import android.Manifest;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.NavUtils;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
-import com.example.usman.proretro.R;
 import com.example.usman.proretro.interfaces.Api;
 import com.example.usman.proretro.models.Location;
 
@@ -37,76 +29,20 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
- * Created by usman on 15/03/2018.
+ * Created by usman on 14/04/2018.
  */
 
-public class FirstActivity extends AppCompatActivity {
-
+public class TrackingOnSide extends AppCompatActivity {
     private BroadcastReceiver broadcastReceiver;
     Toolbar toolbar;
-
-
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_first);
 
-        Button addFu = (Button) findViewById(R.id.btnAddFuel);
-        Button fuelS = (Button) findViewById(R.id.fuel);
-        //String nem = getIntent().getStringExtra("nameForMap");
-
-        toolbar = (Toolbar)findViewById(R.id.refresh);
-        getSupportActionBar().setTitle("Menu");
-
-//        String ides = (String) getIntent().getExtras().get("idMap");
-      //  Toast.makeText(this, ""+ides, Toast.LENGTH_SHORT).show();
-        fuelS.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(FirstActivity.this, FuelActivity.class));
-            }
-        });
-
-        addFu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(FirstActivity.this, AddFuelActivity.class));
-            }
-        });
-
-        Button locat = (Button)findViewById(R.id.btLoc);
-        locat.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(FirstActivity.this, LocationActivity.class));
-            }
-        });
-        Button maps = (Button)findViewById(R.id.btMaps);
-        maps.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(FirstActivity.this, MapsActivity.class));
-            }
-        });
-        Button locates = (Button) findViewById(R.id.btnLocate);
-        locates.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(FirstActivity.this, MapsServiceless.class));
-            }
-        });
-        Button serv = (Button) findViewById(R.id.btnService);
-        serv.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(FirstActivity.this, TrackingOnSide.class));
-            }
-        });
-      /*  Intent i = new Intent(getApplicationContext(), ServiceClass.class);
-        startService(i);*/
+        Intent i = new Intent(getApplicationContext(), ServiceClass.class);
+        startService(i);
     }
-
-  /*  private boolean runtime_permissions() {
+    private boolean runtime_permissions() {
         if (Build.VERSION.SDK_INT >= 23 && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
 
             requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 100);
@@ -140,7 +76,7 @@ public class FirstActivity extends AppCompatActivity {
                     double langi = (double) intent.getExtras().get("lang");
 
                     Calendar calendar = Calendar.getInstance();
-                   // SimpleDateFormat sdf = new SimpleDateFormat("yyyy / MM / dd ");
+                    // SimpleDateFormat sdf = new SimpleDateFormat("yyyy / MM / dd ");
                     SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy ");
                     String strDate = sdf.format(calendar.getTime());
 
@@ -160,11 +96,12 @@ public class FirstActivity extends AppCompatActivity {
         if (broadcastReceiver != null) {
             unregisterReceiver(broadcastReceiver);
         }
+
     }
 
     private void sendNetworkRequest(Location location) {
 
-      //  Retrofit retrofit = new Retrofit.Builder().baseUrl("https://fuel-hero.herokuapp.com/")
+        //  Retrofit retrofit = new Retrofit.Builder().baseUrl("https://fuel-hero.herokuapp.com/")
         Retrofit retrofit = new Retrofit.Builder().baseUrl("https://fuel-hero.herokuapp.com/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
@@ -174,42 +111,13 @@ public class FirstActivity extends AppCompatActivity {
         call.enqueue(new Callback<List<Location>>() {
             @Override
             public void onResponse(Call<List<Location>> call, Response<List<Location>> response) {
-                Toast.makeText(FirstActivity.this, "sendingLoc in db", Toast.LENGTH_SHORT).show();
+                Toast.makeText(TrackingOnSide.this, "sendingLoc in db", Toast.LENGTH_SHORT).show();
             }
             @Override
             public void onFailure(Call<List<Location>> call, Throwable t) {
-                Toast.makeText(FirstActivity.this, "check if goes to db", Toast.LENGTH_SHORT).show();
+                Toast.makeText(TrackingOnSide.this, "check if goes to db", Toast.LENGTH_SHORT).show();
             }
         });
 
-    }*/
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.get_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        String title = (String)item.getTitle();
-        //Toast.makeText(this, "title is: "+title, Toast.LENGTH_SHORT).show();
-
-        switch (item.getItemId()){
-            case R.id.refresh:
-                Toast.makeText(this, "Refreshed :)", Toast.LENGTH_SHORT).show();
-                finish();
-                startActivity(getIntent());
-                break;
-            case android.R.id.home:
-                NavUtils.navigateUpFromSameTask(this);
-                return true;
-
-        }
-
-        return true;
     }
 }
